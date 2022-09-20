dcc1e8b65118e731efde79b90ba419a6b59aa6db
RAIN <64710601+RAIN13-PY@users.noreply.github.com>
2022-09-20 10:53:33 -0500

    Added collisiom

---
+++ scripts/Global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+var points = 0
+var kills = 0
+var health = 3
+
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-var speed = 100
+var speed = 200
 var velocity = Vector2()
 
 func _process(delta):
@@ -11,3 +11,12 @@ func _process(delta):
 	global_position.x = clamp(global_position.x,0, 1024)
 	global_position.y = clamp(global_position.y,0, 600)
 	global_position += speed * velocity * delta
+	if Global.health <= 0:
+		queue_free()
+
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("Enemy"): # Add {and} to add other objects to the list
+		print("Player Hit:" + area.get_name() + ": and did 1 damage") # debug
+		area.queue_free()  # deletes area on impact for more realism. We should make a particle for this death so it looks smoother
+		Global.health -= 1 
