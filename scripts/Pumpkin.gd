eab5d4e00d33781adda8963df0464adca409ee60
RAIN <64710601+RAIN13-PY@users.noreply.github.com>
2022-09-21 10:58:12 -0500

    Added pumpkin spawning

--- scripts/Pumpkin.gd
+++ scripts/Pumpkin.gd
@@ -4,6 +4,6 @@ extends StaticBody2D
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("Player"):
 		Global.Player_points += round(rand_range(50, 150))
-		Global.instance_node(self, Vector2(rand_range(0, 1024), rand_range(0,600)), self)
+		Stage.spawn_pumpkin()
 		queue_free()
 		
--- scripts/Stage.gd
+++ scripts/Stage.gd
@@ -1,16 +1,7 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var pumpkin = preload("res://scenes/Pumpkin.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func spawn_pumpkin():
+	Global.instance_node(pumpkin, Vector2(rand_range(0,1024), rand_range(0, 600)), self)
